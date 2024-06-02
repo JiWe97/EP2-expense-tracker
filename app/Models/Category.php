c27623611efee380738eae38be86dc2d095dc79a
Jill Wets <wetsjill@gmail.com>
2024-06-02 17:57:35 +0200

    Added CategoryRequest and CRUD functionality to categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'color', 'icon'];
+
+    public function toggleShow()
+    {
+        $this->show =!$this->show;
+        $this->save();
+
+        return $this;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,19 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\Request;
 use App\Models\Category;
+use App\Http\Requests\CategoryRequest;
+use App\Models\Budget;
 
 Route::get('/', function () {
     return view('welcome');
 });
-/* 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+})/* ->middleware(['auth', 'verified'])->name('dashboard')*/; 
+/* 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -20,6 +23,47 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php'; */
 
+//Categories
 Route::get('/settings/categories', function () {
-    return view('settings.categories', ['categories' => Category::all()]);
-});
+    return view('settings.categories.categories', ['categories' => Category::all()]);
+})->name('categories');
+
+Route::view('/settings/categories/create', 'settings.categories.create')
+    ->name('categories.create');
+
+Route::get('/settings/categories/{category}', function ($categoryId) {
+    $category = Category::where('name', $categoryId)->first();
+    return view('settings.categories.show', ['category' => $category]);
+})->name('categories.show');
+
+Route::get('/settings/categories/{category}/edit', function (category $category) {
+    return view('settings.categories.edit', ['category' => $category]);
+})->name('categories.edit');
+
+Route::post('/settings/categories', function (CategoryRequest $request) {
+    $category = Category::create($request->validated());
+    return redirect()->route('categories', ['category' => $category->id])
+        ->with('success', 'Category created successfully');
+}) ->name('categories.store');
+
+Route::put('/settings/categories/{category}', function (Category $category, CategoryRequest $request) {
+    $category->update($request->validated());
+    return redirect()->route('categories.show', ['category' => $category->name])
+        ->with('success', 'category updated successfully');
+}) ->name('categories.update');
+
+Route::delete('/settings/categories/{category}', function (category $category) {
+    $category->delete();
+    return redirect()->route('categories')
+        ->with('success', 'category deleted successfully');
+}) ->name('categories.destroy');
+
+Route::put('/settings/categories/{category}/toggle-show', function(Category $category) {
+    $category->toggleShow();
+    return redirect()->back()->with('success', 'Category updated successfully');
+})->name('categories.toggle-show');
+
+//budgets
+Route::get('/settings/budgets', function () {
+    return view('settings.budgets.budgets', ['budgets' => budgets::all()]);
+})->name('budgets');

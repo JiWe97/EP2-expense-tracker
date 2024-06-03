9ecbcc39be63ecd7dedf620c0463d6fec083a173
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-06-03 11:27:10 +0200

    uncomment route profile

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})/* ->middleware(['auth', 'verified'])->name('dashboard')*/;
-/* 
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php'; */
+require __DIR__ . '/auth.php';
 
 //Categories
 Route::get('/settings/categories', function () {

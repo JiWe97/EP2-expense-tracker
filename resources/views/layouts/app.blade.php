98de115800463fa652d27450f106efc0cb0d10ba
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-06-03 11:24:50 +0200

    re-added navigation bar to dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Expense Tracker</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="//unpkg.com/alpinejs" defer></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
         {{-- blade-formatter-disable --}}
         <style type="text/tailwindcss">
@@ -25,9 +31,21 @@
         <div>
         @yield('styles')
     </head>
+<body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @include('layouts.navigation')
 
-    <body class="container mx-auto mt-10 mb-10 max-w-lg">
-        <h1 class="text-2xl mb-4">@yield('title')</h1>
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <!-- Page Content -->
+            <h1 class="text-2xl mb-4">@yield('title')</h1>
         <div x-data="{ flash: true }">
         @if (session()->has('success'))
             <div x-show="flash" class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700" role="alert">
@@ -44,5 +62,10 @@
         </div>
         @endif
         <div>@yield('content')</div>
+            
+        </div>
+    
+        
     </body>
     </html>
+

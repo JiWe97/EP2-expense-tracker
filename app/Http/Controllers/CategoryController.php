b56627765289e7579304a8421164624701adc915
Jill Wets <wetsjill@gmail.com>
2024-06-04 16:35:04 +0200

    db-update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Requests\CategoryRequest;
+use App\Models\User;
 
 class CategoryController extends Controller
 {
@@ -21,6 +22,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
+        
         return view('settings.categories.create');
     }
 
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -24,8 +24,4 @@ class Category extends Model
         return $this->belongsToMany(Budget::class, 'budget_categories', 'category_id', 'budget_id');
     }
 
-    public function user()
-    {
-        return $this->belongsToMany(User::class, 'category_user', 'category_id', 'user_id');
-    }
 }

eb169c4683883fbad3a3310bd33485c238909d42
vickiramsdonck <vicki.ramsdonck@student.pxl.be>
2024-06-24 20:45:17 +0200

    added commenting, removed unneeded code

--- app/Models/BankingRecord.php
+++ app/Models/BankingRecord.php
@@ -9,22 +9,25 @@ class BankingRecord extends Model
 {
     use HasFactory;
 
+    // Define attributes that can be mass assigned
     protected $fillable = [
         'user_id',
         'name',
         'bank_name',
         'balance',
         'account_number',
-        'balance',
     ];
 
+    // Define a relationship with Transaction model
+    // A banking record has many transactions
     public function transactions()
     {
         return $this->hasMany(Transaction::class, 'banking_record_id');
     }
 
 
-
+    // Define a relationship with User model
+    // A banking record belongs to a user
     public function user()
     {
         return $this->belongsTo(User::class);
